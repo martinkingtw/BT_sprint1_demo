d2472c5ea851692c21695855ef11694bf705e8c2
lhlee99 <u3547521@hku.hk>
2019-10-26 01:27:27 +0800

    new pbi using createView template

--- productBacklog/urls.py
+++ productBacklog/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from .views import PBListView, PBDetailView
+from .views import (
+	PBListView,
+	PBDetailView, 
+	PBCreateView
+)
 from . import views
 
 urlpatterns = [
     path('', PBListView.as_view(), name='productBacklog-home'),
     path('<int:pk>/', PBDetailView.as_view(), name='productBacklog-detail'),
+    path('PBI/create', PBCreateView.as_view(), name='productBacklog-create'),
     path('about/', views.about, name='productBacklog-about'),
     path('delete/', views.delete, name='productBacklog-delete'),
-    path('create/', views.create_pbi, name='productBacklog-create'),
 ]
--- productBacklog/views.py
+++ productBacklog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import PBIs
 from .forms import PBIForm
 
@@ -20,6 +20,24 @@ class PBDetailView(DetailView):
 	template_name = 'productBacklog/detail.html'
 	context_object_name = 'PBI'
 
+class PBCreateView(CreateView):
+	model = PBIs
+	# print(form.instance.project_id)
+	fields = [
+			'priority',
+			'title',
+			'status',
+			'ESP',
+			'details'
+	]
+	# def dispatch(self, request, *args, **kwargs):
+	# 	self.project = get_object_or_404(project)
+	# 	return super().dispatch(request, *args, **kwargs)
+
+	def form_valid(self, form):
+		# form.instance.project_id = project.pk
+		return super().form_valid(form)
+
 def about(request):
 	return render(request, 'productBacklog/about.html')
 
@@ -31,14 +49,4 @@ def delete(request):
 	}
 	return render(request, 'productBacklog/home.html', context)
 
-def create_pbi(request):
-	form = PBIForm(request.POST or None)
-	if form.is_valid():
-		form.save()
-		form = PBIForm()
-		return home(request)
 
-	context = {
-		'form': form 
-	}
-	return render(request, 'productBacklog/create.html', context)
