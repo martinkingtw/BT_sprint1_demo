8bf779958d55638f58d2b3e76f1f08a03c770c3d
lhlee99 <u3547521@hku.hk>
2019-11-03 01:20:33 +0800

    better auto-naming

--- sprintBacklog/models.py
+++ sprintBacklog/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 class Sprint(models.Model):
 	title = models.CharField(max_length=20, default="Sprint")
+	sprint_number = models.IntegerField(blank=True, null=True)
 	available_effort = models.IntegerField()
 	start_date = models.DateField(default="date.today")
 	duration = models.IntegerField("duration(week)", default=2)
@@ -19,7 +20,13 @@ class Sprint(models.Model):
 
 	def save(self, *args, **kwargs):
 		number_of_sprint = Sprint.objects.filter(project=self.project).count()
-		self.title = "Sprint" + str(number_of_sprint + 1)
+		if self in Sprint.objects.all():
+			number_of_sprint = self.sprint_number			
+		else:
+			number_of_sprint += 1
+		
+		self.title = "Sprint" + str(number_of_sprint)
+		self.sprint_number = number_of_sprint
 		super().save(*args, **kwargs)
 
 from productBacklog.models import PBI
