b017101cd602695f9ecdf6e038f5947a4d377557
lhlee99 <u3547521@hku.hk>
2019-11-11 14:48:16 +0800

    conflict

--- project/models.py
+++ project/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
 
-# Create your models here.
+# fkelank
 class Project(models.Model):
 	title = models.CharField(max_length=20, unique=True)
 	details = models.TextField()
