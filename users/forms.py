418175ef46872bdea8599c7e9ebc0fdff9cc9cd5
unknown <heatgoli@connect.hku.hk>
2019-11-12 00:24:39 +0800

    add profile system and position

--- users/forms.py
+++ users/forms.py
@@ -5,9 +5,16 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
 	email=forms.EmailField()
+	USER_CHOICE =(
+		(1,("Product Owner")),
+		(2,("Scrum Master")),
+		(3,("Developer"))
+)
+
+	position = forms.ChoiceField(choices = USER_CHOICE,  label="Position", initial='', widget=forms.Select(), required=True)
 
 
 	class Meta:
 		model = User
-		fields = ['username', 'email', 'password1', 'password2']
+		fields = ['username', 'email', 'position','password1', 'password2']
 		
