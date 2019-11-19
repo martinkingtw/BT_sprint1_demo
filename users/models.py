ac4e881db99951c54abb3a1a4c8fef0ee262c5df
martinkingtw <martinkinghello@gmail.com>
2019-11-19 16:33:20 +0800

    minor adjustment on the display of user

--- users/models.py
+++ users/models.py
@@ -64,7 +64,7 @@ class User(AbstractBaseUser):
 	objects = MyAccountManager()
 
 	def __str__(self):
-		return str(self.id)
+		return self.username
 
 	def has_perm(self,perm,obj=None):
 		return self.is_admin
